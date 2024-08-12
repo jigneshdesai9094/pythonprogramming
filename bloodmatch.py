795806b0898d06cf2279c91bef3eaf540e1a6e9d
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-08-12 16:14:50 +0530

    Delete bloodmatch.py

--- bloodmatch.py
+++
@@ -1,6 +0,0 @@
-bool1=input("Enter First Person Blood").upper()
-bool2=input("Enter Second Person Blood").upper()
-if(bool1==bool2):
-    print(bool1," ",bool2," is match")
-else:
-    print("Blood is not match")
