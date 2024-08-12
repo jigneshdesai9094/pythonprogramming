37215241a80efd923cc29268958c42a7103b6aae
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-08-12 10:14:15 +0530

    Add files via upload

---
+++ bloodmatch.py
@@ -0,0 +1,6 @@
+bool1=input("Enter First Person Blood").upper()
+bool2=input("Enter Second Person Blood").upper()
+if(bool1==bool2):
+    print(bool1," ",bool2," is match")
+else:
+    print("Blood is not match")
