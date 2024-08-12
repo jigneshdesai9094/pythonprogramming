57feca9c944e6e9657a3b54700c2518ada2603ef
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-08-12 16:15:08 +0530

    Delete cardlucky.py

--- cardlucky.py
+++
@@ -1,8 +0,0 @@
-c=input("Enter Any Suit with Card : ").lower()
-c=c.strip()
-suit,card=c.split()
-#print(suit," ",card)
-if(suit=='diamond' or (suit=='spade' and card=='ace') or card=='king'):
-  print("Card is Lucky")
-else:
-  print("Please,try next time")
